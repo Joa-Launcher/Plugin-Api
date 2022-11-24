cb03632032fb0e3aaadef4258b29130bdde99cae
FlurinBruehwilerCMI <flurin.bruehwiler@cmiag.ch>
2022-11-24 15:25:22 +0100

    feat(dok): changed documentation [24.11.2022]

--- src/IProviderContext.cs
+++ src/IProviderContext.cs
@@ -1,3 +1,6 @@
 ﻿namespace JoaLauncher.Api;
 
+/// <summary>
+/// Can be used to pass parameters to a provider
+/// </summary>
 public interface IProviderContext { }
--- src/IStepBuilder.cs
+++ src/IStepBuilder.cs
@@ -8,12 +8,19 @@ namespace JoaLauncher.Api;
 public interface IStepBuilder
 {
     /// <summary>
-    /// Can be used to add a Provider to the Step.
+    /// Can be used to add a Provider without a ProviderContext to the Step.
     /// </summary>
-    /// <typeparam name="T"></typeparam>
-    /// <returns></returns>
+    /// <typeparam name="T">The Type of Provider to be added to the StepBuilder</typeparam>
+    /// <returns>The StepBuilder</returns>
     public IStepBuilder AddProvider<T>() where T : IProvider;
     
+    /// <summary>
+    /// Can be used to add a Provider with a ProviderContext to the Step.
+    /// </summary>
+    /// <param name="providerContext">The ProviderContext used to instantiate the Provider</param>
+    /// <typeparam name="TProvider">The Type of Provider to be added to the StepBuilder</typeparam>
+    /// <typeparam name="TContext">The Type of ProviderContext used to instatiate the Provider</typeparam>
+    /// <returns>The StepBuilder</returns>
     public IStepBuilder AddProvider<TProvider, TContext>(TContext providerContext) 
         where TProvider : IProvider<TContext> 
         where TContext : IProviderContext;
