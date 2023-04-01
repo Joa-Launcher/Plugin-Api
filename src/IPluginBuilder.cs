f19a40099a5486401a01e27eb51654fa839761d5
Flurin Brühwiler <bruhwiler.flurin@gmail.com>
2023-04-01 09:33:08 +0200

    feat(SaveAction): changed saveaction methods [01.04.2023]

--- src/IPluginBuilder.cs
+++ src/IPluginBuilder.cs
@@ -42,12 +42,15 @@ public interface IPluginBuilder
     /// <returns></returns>
     public IPluginBuilder AddSaveAction<T>(string nameOfProperty, Action<T> callback) where T : class, ISetting;
 
+
     /// <summary>
-    /// Adds a SaveAction for a specified settings type
-    /// The callback is called when any property on this type changes from outside (from the settings UI or the JSON file)
+    /// Adds a SaveAction for a specified property on a specified settings type
+    /// The callback is called when the property on the type changes from outside (from the settings UI or the JSON file)
     /// </summary>
+    /// <param name="nameOfProperty"></param>
     /// <param name="callback"></param>
     /// <typeparam name="T"></typeparam>
     /// <returns></returns>
-    public IPluginBuilder AddSaveAction<T>(Action<T> callback) where T : class, ISetting;
+    /// <exception cref="NotImplementedException"></exception>
+    public IPluginBuilder AddSaveAction<T>(string nameOfProperty, Func<T, Task> callback) where T : class, ISetting;
 }
